ad388328d91368fd8749d3b5896e29ea8aa4d031
Jen-Chieh <jcs090218@gmail.com>
2021-06-25 14:13:44 +0800

    Add test

---
+++ csproj/src/Main.cs
@@ -0,0 +1,16 @@
+/**
+ * $File: Main.cs $
+ * $Date: 2021-06-25 13:51:51 $
+ * $Revision: $
+ * $Creator: Jen-Chieh Shen $
+ * $Notice: See LICENSE.txt for modification and distribution information
+ *                   Copyright © 2021 by Shen, Jen-Chieh $
+ */
+
+/// <summary>
+///
+/// </summary>
+public class Main
+{
+    
+}

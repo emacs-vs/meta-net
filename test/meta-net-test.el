b018ece490fa7806e1f225bef4a053e70479e62f
JenChieh <jcs090218@gmail.com>
2021-07-11 00:43:37 +0800

    Add test file

---
+++ test/meta-net-test.el
@@ -0,0 +1,8 @@
+;; ========================================================================
+;; $File: meta-net-test.el $
+;; $Date: 2021-07-11 00:38:48 $
+;; $Revision: $
+;; $Creator: Jen-Chieh Shen $
+;; $Notice: See LICENSE.txt for modification and distribution information
+;;                   Copyright © 2021 by Shen, Jen-Chieh $
+;; ========================================================================
